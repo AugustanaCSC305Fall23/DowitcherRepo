eed51b50962e30a2dafb81611716dcc410a87a79
Patrick Mayer <patrickmayer21@augustana.edu>
2023-10-08 18:12:57 -0500

    Initial landing page design with connection to editing page

--- DowitcherGymnasticsSoftware/src/main/java/edu/augustana/LandingPageController.java
+++ DowitcherGymnasticsSoftware/src/main/java/edu/augustana/LandingPageController.java
@@ -10,7 +10,7 @@ public class LandingPageController {
 
     @FXML
     private void switchToCreatePopup() throws IOException {
-        App.setRoot("editing page");
+        App.setRoot("EditingPage");
     }
 
 
