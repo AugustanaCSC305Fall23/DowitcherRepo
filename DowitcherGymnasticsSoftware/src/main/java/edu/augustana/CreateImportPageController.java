5b5d22786feddcba2db1bf460707148bcc353e50
moisess6980 <137333804+moisess6980@users.noreply.github.com>
2023-10-10 11:10:42 -0500

    CreateImportPage java and fxml files have been made, There are no connections yet to the code but the page menu and buttons have been created up to this code commit.

---
+++ DowitcherGymnasticsSoftware/src/main/java/edu/augustana/CreateImportPageController.java
@@ -0,0 +1,17 @@
+package edu.augustana;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CreateImportPageController {
+    @FXML private Button Cancel;
+    @FXML private Button Import;
+    @FXML private Button Create;
+
+    @FXML
+    private void switchToCreatePopup() throws IOException {
+        App.setRoot("EditingPage");
+    }
+
+}
