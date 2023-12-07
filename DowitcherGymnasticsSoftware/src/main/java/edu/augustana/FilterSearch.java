4a614ad026ff840d2cdc3450b6babcdb4ea1c17c
moisess6980 <137333804+moisess6980@users.noreply.github.com>
2023-12-07 11:14:08 -0600

    Fixing and issue where when user manually unchecks the filters an empty list result of cards are returned. Issue is fixed original list is returned.

--- DowitcherGymnasticsSoftware/src/main/java/edu/augustana/FilterSearch.java
+++ DowitcherGymnasticsSoftware/src/main/java/edu/augustana/FilterSearch.java
@@ -1,7 +1,9 @@
 package edu.augustana;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 import edu.augustana.ui.CardUI;
 import javafx.scene.control.CheckBox;
@@ -29,13 +31,21 @@ public class FilterSearch {
     public void applyFilter() {
         List<Card> filteredCards = new ArrayList<>();
         cardImageView.getChildren().clear(); // Clear existing items before adding filtered cards
+
+        boolean anyCheckboxSelected = false;
+
         for (CheckBox checkbox : categoryCheckboxes) {
             if (checkbox.isSelected()) {
+                anyCheckboxSelected = true;
                 String category = checkbox.getText();
-                for (Card card : allCards) {
+                for (Map.Entry<String, Card> entry : cardMap.entrySet()) {
+                    Card card = entry.getValue();
                     for (String level : card.getLevel()) {
-                        if (card.getCategory().equalsIgnoreCase(category) || card.getEvent().equalsIgnoreCase(category) ||
-                                card.getGender().equalsIgnoreCase(category) || level.equalsIgnoreCase(category)) {
+                        if (card.getCategory().equalsIgnoreCase(category) ||
+                                card.getEvent().equalsIgnoreCase(category) ||
+                                card.getGender().equalsIgnoreCase(category) ||
+                                level.equalsIgnoreCase(category)) {
+
                             VBox cardThumbnail = generateCardThumbnail(card);
                             cardThumbnail.setId(card.getCode() + "-" + card.getEvent());
                             cardImageView.getChildren().add(cardThumbnail);
@@ -45,7 +55,18 @@ public class FilterSearch {
                 }
             }
         }
-    }
+
+        // If no checkbox is selected, display all cards
+        if (!anyCheckboxSelected) {
+            for (Map.Entry<String, Card> entry : cardMap.entrySet()) {
+                Card card = entry.getValue();
+                VBox cardThumbnail = generateCardThumbnail(card);
+                cardThumbnail.setId(card.getCode() + "-" + card.getEvent());
+                cardImageView.getChildren().add(cardThumbnail);
+                filteredCards.add(card);
+            }
+        }
+    }   
 
     public void clearFilter() {
         // Clear checkboxes and update cardImageView
@@ -55,12 +76,30 @@ public class FilterSearch {
         applyFilter();
         cardImageView.getChildren().clear();
 
-        // Iterate through the values in the cardMap
-        for (Card card : cardMap.values()) {
+        // Iterate through the original order of entries in cardMap
+        for (Map.Entry<String, Card> entry : cardMap.entrySet()) {
+            Card card = entry.getValue();
             VBox cardThumbnail = generateCardThumbnail(card);
             cardImageView.getChildren().add(cardThumbnail);
         }
     }
+
+
+
+//    public void clearFilter() {
+//        // Clear checkboxes and update cardImageView
+//        for (CheckBox checkbox : categoryCheckboxes) {
+//            checkbox.setSelected(false);
+//        }
+//        applyFilter();
+//        cardImageView.getChildren().clear();
+//
+//        // Iterate through the values in the cardMap
+//        for (Card card : cardMap.values()) {
+//            VBox cardThumbnail = generateCardThumbnail(card);
+//            cardImageView.getChildren().add(cardThumbnail);
+//        }
+//    }
 
 
     private VBox generateCardThumbnail(Card card) {
